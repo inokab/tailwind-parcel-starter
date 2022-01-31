33cb89100a77f2f86e891f13eb5c12adfd1601f7
inokab <inokabalazs@gmail.com>
2022-01-31 11:06:40 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'montserrat': ['Montserrat', 'sans-serif'],
+        'poppins': ['Poppins', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
